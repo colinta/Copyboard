79ee08cd07b6f3580953272b515cdf78064be6d2
Colin T.A. Gray <colinta@gmail.com>
2018-04-04 09:27:19 -0600

    support getting the clipboard before sending it

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -21,9 +21,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.makeKeyAndVisible()
         self.window = window
 
-        sendClipboard()
+        checkClipboard()
 
         return true
+    }
+
+    func checkClipboard() {
+        let ref = Database.database().reference().child("clipboard/send")
+        ref.observeSingleEvent(of: .value) { snapshot, error in
+            if let value = snapshot.value as? String {
+                UIPasteboard.general.string = value
+                ref.removeValue { _, _ in
+                    exit(0)
+                }
+            }
+            else {
+                self.sendClipboard()
+            }
+        }
     }
 
     func sendClipboard() {
@@ -38,4 +53,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
